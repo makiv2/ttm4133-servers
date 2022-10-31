e5c0e8f26dca6a4d68377821c870f5ff29241c0d
Matija Popovic <matijap@stud.ntnu.no>
2022-10-31 01:39:27 +0100

    buttons at /

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,29 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ButtonComponent from "./components/Button";
 
-import { Link } from "react-router-dom"
-
-
+import { Link } from "react-router-dom";
 
 export default function App() {
-
   return (
-    
     <nav>
-        <Link style={{ color: "black" }} to="/lab0">Lab0</Link>
-        <Link style={{ color: "black", }} to="/lab1">Lab1</Link>
-        <Link style={{ color: "black", }} to="/lab2">Lab2</Link>
-        <Link style={{ color: "black", }} to="/lab3">Lab3</Link>
-        <Link style={{ color: "black", }} to="/lab4">Lab4</Link>
+      <Link style={{ color: "black" }} to="/lab0">
+        <ButtonComponent name="Lab 0"></ButtonComponent>
+      </Link>
+      <Link style={{ color: "black" }} to="/lab1">
+        <ButtonComponent name="Lab 1"></ButtonComponent>
+      </Link>
+      <Link style={{ color: "black" }} to="/lab2">
+        <ButtonComponent name="Lab 2"></ButtonComponent>
+      </Link>
+      <Link style={{ color: "black" }} to="/lab3">
+        <ButtonComponent name="Lab 3"></ButtonComponent>
+      </Link>
+      <Link style={{ color: "black" }} to="/lab4">
+        <ButtonComponent name="Lab 4"></ButtonComponent>
+      </Link>
     </nav>
-
-  )
+  );
 }
 
 //export default App;
