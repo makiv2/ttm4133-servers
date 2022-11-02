85226e6a5a2c1fe20eaeaed0c88a4784c1163ff8
Matija Popovic <matijap@stud.ntnu.no>
2022-11-02 22:08:54 +0100

    copy to other labs

--- react-app/src/pages/lab4.js
+++ react-app/src/pages/lab4.js
@@ -1,38 +1,53 @@
 import React from "react";
 import { useState, useRef, useContext } from "react";
-import {
-  Stage,
-  Layer,
-} from "react-konva";
+import { Stage, Layer } from "react-konva";
 import ButtonComponent from "../components/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Form from "react-bootstrap/Form";
 import ClickContext from "../context/ClickContext";
 import { useAddItem } from "../hooks/useAddItem";
 import AddContext from "../context/AddContext";
-import SaveButton from "../components/SaveButton"
-
+import SaveButton from "../components/SaveButton";
+import UeObject from "../components/ueObject/ueObject";
 
 import "../App.css";
-
-var width = window.innerWidth *0.70;
-var heigth = window.innerHeight *0.70;
+import StageGroup from "../components/stage/stage";
 
 export default function Defaultlab() {
   const [name, setName] = useState("");
   const [txPower, setTxPower] = useState(0);
-  const [ueList, setueList, 
-    eNBList, seteNBList, 
-    buildingList, setbuildingList, 
-    ueListRefs, setueListRefs, 
-    eNBListRefs, seteNBListRefs, 
-    buildingListRefs, setbuildingListRefs,
-    radiusRefs, setRadiusRefs,
-    radius, setRadius] = useContext(AddContext)
-    const [clickUe, setClickUe, clickNB, setClickeNB, clickBuilding, setClickBuilding] = useContext(ClickContext);
-    const stageRef = useRef(null);
-    useAddItem(); // Run our custom hook to add items.
-
+  const {
+    ueList,
+    setueList,
+    eNBList,
+    seteNBList,
+    buildingList,
+    setbuildingList,
+    ueListRefs,
+    setueListRefs,
+    eNBListRefs,
+    seteNBListRefs,
+    buildingListRefs,
+    setbuildingListRefs,
+    radiusRefs,
+    setRadiusRefs,
+    radius,
+    setRadius,
+    mobileUeList,
+    mobileUeListRefs,
+  } = useContext(AddContext);
+  const [
+    clickUe,
+    setClickUe,
+    clickNB,
+    setClickeNB,
+    clickBuilding,
+    setClickBuilding,
+    clickMobileUe,
+    setClickMobileUe,
+  ] = useContext(ClickContext);
+  const stageRef = useRef(null);
+  useAddItem(); // Run our custom hook to add items.
 
   //const [mobileUE] = useImage('https://konvajs.org/assets/lion.png');
 
@@ -43,8 +58,8 @@ export default function Defaultlab() {
   const updateRadius = (e) => {
     setRadius(e.target.value);
 
-    radiusRefs.map((node) => {
-      node.setRadius(e.target.value * 10);
+    radiusRefs.forEach((node) => {
+      node.setRadius(e.target.value * 5);
     });
 
     setTxPower(e.target.value); //todo
@@ -53,44 +68,34 @@ export default function Defaultlab() {
   const testFunction = () => {
     console.log(name);
   };
-
+  //scale on layer
   return (
     <div>
-      <Stage ref={stageRef} width={width} height={heigth}>
-        <Layer>
-          {buildingList.map((e) => {
-            return e;
-          })}
+      <StageGroup
+        ueList={ueList}
+        eNBList={eNBList}
+        buildingList={buildingList}
+        mobileUeList={mobileUeList}
+        stageRef={stageRef}
+      />
 
-          {eNBList.map((e) => {
-            return e;
-          })}
-
-          {ueList.map((e) => {
-            return e;
-          })}
-
-          {/* <Circle radius={radius} stroke="black" strokeWidth={5} x={50} y={50} draggable /> */}
-        </Layer>
-        <Layer name="top-layer" />
-      </Stage>
-
-
-      <ButtonComponent name="Add UE" onClick={() => setClickUe(true)}/>
-      <ButtonComponent name="Add eNB" onClick={() => setClickeNB(true)}/>
-      <ButtonComponent name="Add building" onClick={() => setClickBuilding(true)}/>
-      {/* <ButtonComponent name="Add Mobile UE" onClick={useAddBuildingItem}/> */}
-      <ButtonComponent name="Clear" onClick={clearItems}/>
-      <ButtonComponent name="Default Topology" onClick={testFunction}/>
-
-
-
+      <ButtonComponent name="Add UE" onClick={() => setClickUe(true)} />
+      <ButtonComponent name="Add mobile UE path" onClick={() => setClickMobileUe(true)} />
+      <ButtonComponent name="Add eNB" onClick={() => setClickeNB(true)} />
+      <ButtonComponent
+        name="Add building"
+        onClick={() => setClickBuilding(true)}
+      />
+      <ButtonComponent name="Clear" onClick={clearItems} />
+      <ButtonComponent name="Default Topology" onClick={testFunction} />
 
       <Form.Label>TxPower: </Form.Label>
 
       <input value={radius} className="btn-custom" onChange={updateRadius} />
-      <Form.Text className="text-muted"> Values from 38 to 43 dBm are accepted. </Form.Text>
-
+      <Form.Text className="text-muted">
+        {" "}
+        Values from 38 to 43 dBm are accepted.{" "}
+      </Form.Text>
 
       <div className="container-fluid page-layout spaceTop">
         <div className="row">
@@ -98,20 +103,37 @@ export default function Defaultlab() {
 
           <div className="col-md-2">
             <div className="container-fluid">
-                <Form.Label>Simulation Name</Form.Label>
-                <Form.Control
-                  type="text"
-                  placeholder="Enter name"
-                  value={name}
-                  className="btn-custom"
-                  onChange={(event) => setName(event.target.value)}
-                />
-                  <Form.Text className="text-muted"> Don't choose the same names as previous simulations. </Form.Text>
+              <Form.Label>Simulation Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter name"
+                value={name}
+                className="btn-custom"
+                onChange={(event) => setName(event.target.value)}
+              />
+              <Form.Text className="text-muted">
+                {" "}
+                Values from 38 to 43 dBm are accepted.{" "}
+              </Form.Text>
 
-                  <SaveButton axiosLink="/uploadCoords" axiosLinkImage="/uploadImageBase64" label="4" name={name} stageRef={stageRef} eNBListRefs={eNBListRefs} buildingListRefs={buildingListRefs} ueListRefs={ueListRefs}/>
+              <SaveButton
+                axiosLink="/uploadCoords"
+                axiosLinkImage="/uploadImageBase64"
+                label="0"
+                name={name}
+                stageRef={stageRef}
+                eNBListRefs={eNBListRefs}
+                buildingListRefs={buildingListRefs}
+                ueListRefs={ueListRefs}
+                mobileUeListRefs={mobileUeListRefs}
+              />
             </div>
           </div>
-          <div className="col-md-9 spaceTop"> Make sure to rememberd the name of the simulation as you need it later when you want to run it.</div>
+          <div className="col-md-9 spaceTop">
+            {" "}
+            Make sure to rememberd the name of the simulation as you need it
+            later when you want to run it.
+          </div>
         </div>
       </div>
     </div>
